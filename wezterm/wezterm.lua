3a774f40856f1aaa0a4ba824502d4e7fe6276cff
Luuk Lelifeld <luukleli2@hotmail.com>
2024-11-16 20:24:32 +0100

    wezterm keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,6 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-local is_mac = function()
-    return wezterm.target_triple:find("darwin") ~= nil
-end
-
 local is_windows = function()
     return wezterm.target_triple:find("windows") ~= nil
 end
@@ -13,7 +9,8 @@ if is_windows then
     config.wsl_domains = {
         {
             name = 'WSL:Ubuntu',
-            distribution = 'Ubuntu'
+            distribution = 'Ubuntu',
+			default_cwd = '~'
         }
     }
 
@@ -22,33 +19,29 @@ end
 
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'Catppuccin Mocha'
-
 config.window_decorations = 'TITLE | RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 160
+config.initial_rows = 40
 
 local leader = 'CTRL'
 
-if is_mac then
+if not is_windows then
     leader = 'CMD'
 end
 
 config.leader = {key = 'a', mods = leader, timeout_milliseconds = 1000}
+act = wezterm.action
+
 config.keys = {
-  {
-    mods   = "LEADER",
-    key    = "v",
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-  },
-  {
-    mods   = "LEADER",
-    key    = "h",
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  },
-  {
-    mods = "LEADER",
-    key = "x",
-    action = wezterm.action.CloseCurrentPane { confirm = true }
-  }
+  { mods = 'LEADER', key = 'r', action = act.ReloadConfiguration },
+  { mods = 'LEADER', key = 'f', action = act.ToggleFullScreen },
+  { mods = 'LEADER', key = 't', action = act.SpawnTab 'CurrentPaneDomain' },
+  { mods = 'LEADER', key = 'X', action = act.CloseCurrentTab { confirm = true } },
+  { mods = "LEADER", key = "v", action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { mods = "LEADER", key = "h", action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { mods = "LEADER", key = "x", action = act.CloseCurrentPane { confirm = true } },
+  { mods = 'LEADER', key = 's', action = act.PaneSelect { alphabet = 'arstgmneio' } }
 }
 
 return config
