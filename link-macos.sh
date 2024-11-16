8c9bbd4d73b45fb52a13ec81a8cdf7709610c4b1
Luuk Lelifeld <luuklelifeld@luuklelifeld.local>
2024-11-16 12:11:07 +0100

    restructure, add linking script, wezterm and zsh additions

---
+++ link-macos.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+script_dir="$(dirname $0)"
+
+if [ $script_dir = "." ]
+then
+    script_dir=""
+else
+    script_dir="/$script_dir"
+fi
+
+path="$(pwd)$script_dir"
+
+# Neovim
+mkdir ~/.config
+mkdir ~/.config/nvim
+ln -s "$path/nvim/init.lua" ~/.config/nvim/init.lua
+ln -s "$path/nvim/lua" ~/.config/nvim/lua
+
+# oh-my-zsh
+mkdir ~/.oh-my-zsh
+mkdir ~/.oh-my-zsh/custom
+ln -s "$path/zsh/.zshrc" ~/.zshrc
+ln -s "$path/zsh/.oh-my-zsh/custom/aliases.zsh" ~/.oh-my-zsh/custom/aliases.zsh
+
+# Wezterm
+ln -s "$(pwd)$script_dir/wezterm/.wezterm.lua" ~/.wezterm.lua
+
---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+local is_mac = function()
+    return wezterm.target_triple:find("darwin") ~= nil
+end
+
+config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = 'Catppuccin Mocha'
+
+config.window_decorations = 'TITLE | RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+
+local leader = 'CTRL'
+
+if is_mac then
+    leader = 'CMD'
+end
+
+config.leader = {key = 'a', mods = leader, timeout_milliseconds = 1000}
+config.keys = {
+  {
+    mods   = "LEADER",
+    key    = "sv",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods   = "LEADER",
+    key    = "=",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  }
+}
+
+return config
