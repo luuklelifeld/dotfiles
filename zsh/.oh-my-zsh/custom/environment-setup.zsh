801bf08e5a8f7c40e5a320565c409e05ffde502c
Luuk Lelifeld <luuk@publitas.com>
2024-12-17 16:20:36 +0100

    feat: environment

--- zsh/.oh-my-zsh/custom/environment-setup.zsh
+++ zsh/.oh-my-zsh/custom/environment-setup.zsh
@@ -1,4 +1,11 @@
-eval "$(jenv init -)"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:PATH"
 eval "$(pyenv init -)"
+
+eval "$(jenv init -)"
+
+export BREW_FORBIDDEN_FORMULAE="node"
