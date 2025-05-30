be0559bc91db5bd0ffa4eb576ad21e4906d2b478
luuklelifeld <luuklelifeld@gmail.com>
2025-05-30 12:55:24 +0200

    chore: cleanup autocomplete files

--- nvim/lua/mappings/init.lua
+++ nvim/lua/mappings/init.lua
@@ -1,8 +1,8 @@
---require("mappings/autocomplete")
 require("mappings/navigation")
 require("mappings/lsp")
 require("mappings/git")
 
+-- Disabled because hardtime.nvim does this as well
 --vim.keymap.set('i', '<Up>', '<Nop>')
 --vim.keymap.set('i', '<Down>', '<Nop>')
 --vim.keymap.set('i', '<Left>', '<Nop>')
--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -1,4 +1,5 @@
 return {
+    -- this is equivalent to setup({}) function
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
     dependencies = { 'rafamadriz/friendly-snippets' },
@@ -19,48 +20,3 @@ return {
     },
     opts_extend = { 'sources.default' }
 }
---return {
---    "hrsh7th/nvim-cmp",
---    dependencies = {
---        "hrsh7th/cmp-nvim-lsp",
---        "hrsh7th/cmp-buffer",
---        "hrsh7th/cmp-path",
---        "hrsh7th/cmp-cmdline",
---        "saadparwaiz1/cmp_luasnip"
---    },
---    config = function()
---        local cmp = require("cmp")
---        cmp.setup({
---            snippet = {
---                expand = function(args)
---                    require("luasnip").lsp_expands(args.body)
---                end
---            },
---            window = {
-----               completion = cmp.config.window.bordered(),
---               documentation = cmp.config.window.bordered()
---            },
---            mapping = {}, -- move this to mappings module
---            sources = cmp.config.sources({
---                {name = "nvim_lsp"},
---                {name = "luasnip"},
---                {name = "buffer"}
---            })
---        })
---
---        cmp.setup.cmdline("/", {
---            mapping = cmp.mapping.preset.cmdline(),
---            sources = {
---                {name = "buffer"}
---            }
---        })
---
---        cmp.setup.cmdline(":", {
---            mapping = cmp.mapping.preset.cmdline(),
---            sources = {
---                {name = "path"},
---                {name = "cmdline"}
---            }
---        })
---    end
---}
