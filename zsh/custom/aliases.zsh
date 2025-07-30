aa5ed8a82ba77fef6a437c4e32523af72b7f60bd
luuklelifeld <luuklelifeld@gmail.com>
2025-07-30 19:58:59 +0200

    chore: new alias + fzf setup

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
-alias vim="nvim"
-
+alias activate="source .venv/bin/activate"
 alias cath="pygmentize -P style=catppuccin-mocha"
+alias hosts-reload="dscacheutil -flushcache"
+alias vim="nvim"
--- zsh/custom/environment-setup.zsh
+++ zsh/custom/environment-setup.zsh
@@ -1,6 +1,8 @@
+source <(fzf --zsh)
+
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:PATH"
